7d38158e73ccf54db2331c96f9d38ea285b8fcaa
test_git <sshabinskiy@mail.ru>
2021-04-03 20:37:34 +0300

    +dockerfile move  files to directories

--- app/main.py
+++ app/main.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    f=open('./data/data.txt','r')
-    return f.read()
+    #'f=open('../data/data.txt', 'r')
+    test='Hello world'
+    return test
 
 if __name__ == '__main__':
     app.run(debug=True)
